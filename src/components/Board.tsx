733bb551325b832d6cb3821f4ec485b694397204
Patrick Wertal <patrick.wertal@gmail.com>
2021-11-26 19:26:22 +0100

    todo fix ctx canvas

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import {Button, ListGroup, Row} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BlockChain } from '../data-models/chain-models';
-import { ShapeNode } from '../data-models/index-models';
+import { ShapeNode ,Style} from '../data-models/index-models';
 import { getEmitHelpers } from 'typescript';
-import Canvas from './Canvas' ;
 
 
 interface BlockChainProps {
@@ -14,57 +13,46 @@ interface BlockChainProps {
  }
 
 const Board : React.FC<BlockChainProps> = (props : BlockChainProps) =>  {
-    const [chain   , setChain]    =  useState<BlockChain>(null) ;
-    const [amount  , setAmount]   =  useState<number[]>(null)   ;
-    const [render, setRerender]   = useState(false);
+        const [chain   , setChain]    =  useState<BlockChain>(null) ;
+        const [amount  , setAmount]   =  useState<number[]>(null)   ;
+        const [render, setRerender]   = useState(false);
 
-    let isLoaded : Boolean = false ;
-    let htmlBlockInfo : string     ;
+        let isLoaded : Boolean = false ;
+        let htmlBlockInfo : string     ;
 
-      useEffect( () => {
+useEffect( () => {
         setChain(props.blockChain) ;
         setAmount(props.amounts)   ;
         setRerender(!render) ;
         //const interval_id = setInterval(() => reurn), 5000);
         //return () => clearInterval(interval_id)
-       },[]) ;
+},[]) ;
 
 
-    const refreshControls = () => {
+const refreshControls = () => {
         alert("Here comes setinterval");
         setChain(props.blockChain) ;
         setAmount(props.amounts)   ;
         setRerender(!render) ;
-    }
-
-    const draw = (ctx : any, frameCount: number) => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        ctx.fillStyle = '#666666'
-        ctx.beginPath()
-        ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-        ctx.fill()
-      }
-
-    return (
-        <div>
-        <Row>
-            <div id="DrawBoard" >      
-            <h3> Graph Component</h3>
-            <h5> {chain && chain.Chainname} </h5> 
-            { chain && chain.getallValues()
-                .map( (item, index) => ( 
-                <Button className="mt-1" variant={item.style} onClick={()=> { alert(item.amount)} }> 
-                {isLoaded ? htmlBlockInfo : item.label} </Button>))}
-            </div>
-            <ListGroup>
-            { chain && chain.getallValues().map( (item,i)  => ( <ListGroup.Item> {item.label}</ListGroup.Item>))}
-        </ListGroup>
-        </Row>
+}
 
 
-
-        <Canvas draw={draw} width={400}  height={280} > </Canvas>
-     </div>)
+return (
+    <div>
+    <Row>
+    <div id="DrawBoard" >      
+    <h3> Graph Component</h3>
+    <h5> {chain && chain.Chainname} </h5> 
+        { chain && chain.getallValues()
+        .map( (item, index) => ( 
+        <Button className="mt-1" variant={item.style} onClick={()=> { alert(item.amount)} }> 
+        {isLoaded ? htmlBlockInfo : item.label} </Button>))}
+        </div>
+    <ListGroup>
+        { chain && chain.getallValues().map( (item,i)  => ( <ListGroup.Item> {item.label}</ListGroup.Item>))}
+    </ListGroup>
+    </Row>
+    </div>)
 
 }
                 
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,51 +1,57 @@
+import { render } from '@testing-library/react';
 import React, { useRef,useEffect } from 'react'
+import {Button} from 'react-bootstrap';
 import { Context } from 'vm'
+import {Style} from '../data-models/index-models' ;
 
 
 interface PropsCanvas {
-    width  : number ;
-    height : number ; 
-    styles? : {}     ;
-    draw   : ( ctx : Context , frameCount: number) => void
-}
+    width   : number  ,
+    height  : number  , 
+    styles? : {}      ,
+    draw        : ( ctx : Context , frameCount: number) => void,
+    drawNode    : ( ctx : Context ) => void
+    };
+
 const Canvas : React.FC<PropsCanvas> = (props : PropsCanvas) =>  {
   
     const canvasRef = useRef(null)
     const styles = {
-         border: "1px solid black" 
-      }
-  
-    
+                    border: "1px solid black" 
+                    }
+    let ctx : any  = null ;
+   
     useEffect(() => {
+      const canvas    = canvasRef.current        ;
+      const context   = canvas.getContext('2d')  ;
+      let frameCount  = 0                        ;
+      let animationFrameId : any                 ;
       
-      const canvas = canvasRef.current
-      const context = canvas.getContext('2d')
-      let frameCount = 0
-      let animationFrameId : any ;
 
-
-      const draw = (ctx : Context, frameCount: number) => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        ctx.fillStyle = '#666666'
-        ctx.beginPath()
-        ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-        ctx.fill()
-      }
-      
       //Our draw came here
       const render = () => {
-        frameCount++
-        props.draw(context, frameCount) ;
-        animationFrameId = window.requestAnimationFrame(render)
+          frameCount++
+          props.draw(context, frameCount) ;
+          animationFrameId = window.requestAnimationFrame(render)
+           paint(context)
+     
       }
-      render()
-
+      render() ;
       return () => {
         window.cancelAnimationFrame(animationFrameId)
       }
-    }, [])
-    
-    return <canvas id="PaintBoard" ref={canvasRef} styles={{color: "red" ,border : "1px solid red"}} {...props}/>
-  }
+    }, []);
+    const paint = (ctx : any) => {
+        props.drawNode(ctx) ;
+    }
+
+    return ( 
+      <div>
+        <canvas id="PaintBoard" ref={canvasRef} style={{color: "red" , border : "1px solid red"}} {...props}/>   
+        <Button variant={Style.Dark} onClick={(e)=> paint(e)} > SORT</Button>
+      </div> 
+        )
+
+}
 
 export default Canvas
